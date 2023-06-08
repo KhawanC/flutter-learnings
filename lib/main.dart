441092c48432d3911e03628d5d927e3236909b32
KhawanC <kauacassiano121@gmail.com>
2023-06-08 00:33:28 -0300

    Criando app bar, tiles, logica de cada lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,10 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 // ignore_for_file: prefer_const_constructors
 
-void main() => runApp(const MaterialApp(
-      home: Aplicativo(),
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
     ));
-
-class Aplicativo extends StatelessWidget {
-  const Aplicativo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-
-
-      appBar: AppBar(
-        title: Text(
-          "Eclipse lunar",
-          style: TextStyle(
-              fontSize: 24,
-              letterSpacing: 5,
-              fontWeight: FontWeight.bold,
-              color: Colors.yellow,
-              fontFamily: "Quicksand"),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-
-
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.green,
-                child: Text("Hello"),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                color: Colors.grey,
-                child: Text("Row"),
-              ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.all(30),
-            color: Colors.pink,
-            child: Text("Hello Column 2"),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.cyan,
-            child: Text("Hello Column"),
-          ),
-          Container(
-            padding: EdgeInsets.all(40),
-            color: Colors.yellow,
-            child: Text("Hello Column 3"),
-          ),
-        ],
-      ),
-
-
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () {},
-        child: Text("click"),
-      ),
-    );
-  }
-}
